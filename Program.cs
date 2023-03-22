a5285c40c6411f40beee5863d63502ca8c233efd
Venkatesh-Kolluri <118035002+Venkatesh-Kolluri@users.noreply.github.com>
2023-03-22 09:56:07 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            MonthlyWage.employeeMonthlyWage();
+            UsingSwitchCase.switchCase();
         }
     }
 }
---
+++ UsingSwitchCase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    internal class UsingSwitchCase
+    {
+        public static void switchCase()
+        {
+            int WAGE_PER_HR = 20;
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int empHrs = 0;
+            int empWage = 0;
+
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            switch (empCheck)
+            {
+                case 1:
+                    empHrs = 8;
+                    break;
+                case 2:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * WAGE_PER_HR;
+            Console.WriteLine("Emp Wage: " + empWage);
+        }
+    }
+}
